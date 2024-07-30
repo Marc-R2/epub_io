87cd86bddc9dd370f86dd56755fac39dca963d08
Marc Renken <marc.renken@bitbot.eu>
2024-07-30 07:58:41 +0200

    feat(EpubTextContentFileRef): auto-detect encoding when utf-8 fails

--- lib/src/ref_entities/epub_text_content_file_ref.dart
+++ lib/src/ref_entities/epub_text_content_file_ref.dart
@@ -1,5 +1,7 @@
 import 'dart:convert' as convert;
+import 'dart:convert';
 
+import 'package:charset/charset.dart';
 import 'package:epub_io/epub_io.dart';
 import 'package:epub_io/src/ref_entities/epub_content_file_ref.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -20,6 +22,27 @@ class EpubTextContentFileRef
 
   Future<String> readContent() async {
     final contentStream = getContentStream();
-    return convert.utf8.decode(contentStream);
+    try {
+      return convert.utf8.decode(contentStream);
+    } catch (e) {
+      final sb = StringBuffer('Error reading $fileName as $contentMimeType');
+      if (e is FormatException) {
+        // Try to detect the encoding
+        final encoding = detectEncoding(contentStream);
+        if (encoding != null) return encoding.decode(contentStream);
+        sb.write(' and failed to detect encoding');
+      }
+      sb.write(': $e');
+      throw Exception(sb.toString());
+    }
+  }
+
+  static Encoding? detectEncoding(List<int> bytes) {
+    final charset = Charset.detect(
+      bytes,
+      orders: [ascii, eucJp, latin1, utf8, eucKr, shiftJis, gbk, utf16, utf32],
+    );
+    if (charset == null) return null;
+    return convert.Encoding.getByName(charset.name);
   }
 }
